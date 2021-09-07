a7eb39be7b31243c66457ba3a07dc0804e9c734e
dimamik <57190429+dimamik@users.noreply.github.com>
2021-09-07 13:13:43 +0200

    Changed HostConfig

--- ui/src/services/HostConfig.js
+++ ui/src/services/HostConfig.js
@@ -1,4 +1,4 @@
-// export const REC_API = "http://185.46.11.241:5001";
-export const REC_API = "http://localhost:5001";
-// export const SEARCH_API = "http://185.46.11.241:5000";
-export const SEARCH_API = "http://localhost:5000";
+export const REC_API = "http://185.46.11.241:5001";
+// export const REC_API = "http://localhost:5001";
+export const SEARCH_API = "http://185.46.11.241:5000";
+// export const SEARCH_API = "http://localhost:5000";
